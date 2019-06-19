27c30674eb1ad54f5bcaf87ffde554e5a1b6d2b5
vmai <vmai@inovex.de>
2019-06-19 14:09:35 +0200

    add queries

--- snake/simulation.go
+++ snake/simulation.go
@@ -1,13 +1,14 @@
 package snake
 
-func simulate(pg [][]int, len int) bool {
+func Simulate(pg Playground, len int, s Snake) bool {
 
 	// TODO: simulate here
+
 
 	len = len - 1
 	if len <= 0 {
 		return true
 	} else {
-		return simulate(pg, len)
+		return Simulate(pg, len, s)
 	}
 }
