31f2d2f4be0c52f411aecb2656fd12cbd63b90ff
vmai <vmai@inovex.de>
2019-06-19 16:06:29 +0200

    add TODOs

--- snake/simulation.go
+++ snake/simulation.go
@@ -1,9 +1,11 @@
 package snake
 
+// TODO: correct input
 func Simulate(pg Playground, len int, s Snake) bool {
 
 	// TODO: simulate here
-
+	// TODO: COPY ALL REFERENCES
+	// TODO: SIMULATE GO TO FOOD
 
 	len = len - 1
 	if len <= 0 {
