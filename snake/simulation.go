1279792c9fb8ef4d757b88a826ea79d244e10776
Marcel Reineck <reineck@hm.edu>
2019-06-20 20:15:17 +0200

    Added simulation, it now simulates up to a given depth

--- snake/simulation.go
+++ snake/simulation.go
@@ -10,14 +10,30 @@ import (
 func Simulate(sc *SimpleSnakeController, move []DIRECTION, snakeLength int) bool {
 	//nolint
 	if len(move) < 1 {
-		sc.Pg.Print()
 		return false
+	} else if len(move) == 2 && depth < 3 {
+		copied := deepcopy.Copy(sc)
+		copyController := copied.(*SimpleSnakeController)
+		err := copier.Copy(copyController.Pg, sc.Pg)
+		if err != nil {
+			fmt.Println("Error occurred while copying")
+		}
+		duplicate := copyController.Pg.CopyPlayGround(copyController.Pg.GetPlayGround())
+		copyController.Snake.len = sc.Snake.len
+		copyController.Snake.LastDirection = sc.Snake.LastDirection
+		if Simulate(copyController, move[:1], depth+1, snakeLength) {
+			sc.Pg.SetPlayGround(duplicate)
+			sc.moveSnakeToFood(move[:1])
+		} else {
+			sc.Pg.SetPlayGround(duplicate)
+			sc.moveSnakeToFood(move[1:])
+		}
+	} else {
+		sc.moveSnakeToFood(move)
 	}
-	sc.moveSnakeToFood(move)
 	snake := sc.Snake
 
 	if sc.Pg.GetContent(snake.Head.X, snake.Head.Y) == BORDER {
-		sc.Pg.Print()
 		return false
 	} else if sc.Pg.GetContent(snake.Head.X, snake.Head.Y) == FOOD {
 		sc.Pg.setRandomFood()
@@ -26,10 +42,9 @@ func Simulate(sc *SimpleSnakeController, move []DIRECTION, snakeLength int) bool
 	sc.Pg.CreateSnake(snake)
 
 	if snakeLength < 1 {
-		//sc.Pg.Print()
 		return true
 	} else {
-		return Simulate(sc, GetDirections(sc.Pg.GetPlayGround()), snakeLength - 1)
+		return Simulate(sc, GetDirections(sc.Pg.GetPlayGround()), depth, snakeLength-1)
 	}
 	return Simulate(sc, GetDirections(sc.Pg.GetPlayGround()), depth, snakeLength-1)
 }
