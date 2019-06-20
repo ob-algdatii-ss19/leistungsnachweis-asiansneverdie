de785d71cb78b2489ffcb397ee18b2c08f7ad1c3
Marcel Reineck <reineck@hm.edu>
2019-06-20 20:15:17 +0200

    Added simulation, it now simulates up to a given depth

--- snake/simulation.go
+++ snake/simulation.go
@@ -7,35 +7,34 @@ import (
 	"github.com/mohae/deepcopy"
 )
 // TODO: correct input
-func Simulate(sc *SimpleSnakeController, move []DIRECTION, snakeLength int) bool {
+func Simulate(sc *SimpleSnakeController, move []DIRECTION, depth, snakeLength int) bool {
 	//nolint
 	if len(move) < 1 {
 		return false
 	} else if len(move) == 2 && depth < 3 {
 		copied := deepcopy.Copy(sc)
 		copyController := copied.(*SimpleSnakeController)
-		err := copier.Copy(copyController.pg, sc.pg)
+		err := copier.Copy(copyController.Pg, sc.Pg)
 		if err != nil {
 			fmt.Println("Error occurred while copying")
 		}
-		duplicate := copyController.pg.CopyPlayGround(copyController.pg.GetPlayGround())
+		duplicate := copyController.Pg.CopyPlayGround(copyController.Pg.GetPlayGround())
 		copyController.Snake.len = sc.Snake.len
 		copyController.Snake.LastDirection = sc.Snake.LastDirection
-		nextStep := sc.GetNextMovableFoodDirection(move)
-		nextArray := []DIRECTION{nextStep}
-		if Simulate(copyController, nextArray, depth+1, snakeLength) {
-			sc.pg.SetPlayGround(duplicate)
-			sc.moveSnakeToFood(nextArray)
+		if Simulate(copyController, move[:1], depth+1, snakeLength) {
+			sc.Pg.SetPlayGround(duplicate)
+			sc.moveSnakeToFood(move[:1])
 		} else {
-			sc.pg.SetPlayGround(duplicate)
-			sc.moveSnakeToFood(Remove(move, nextStep))
+			sc.Pg.SetPlayGround(duplicate)
+			sc.moveSnakeToFood(move[1:])
 		}
 	} else {
 		sc.moveSnakeToFood(move)
 	}
 	snake := sc.Snake
 
-	if sc.pg.GetContent(snake.Head.X, snake.Head.Y) == BORDER {
+	if sc.Pg.GetContent(snake.Head.X, snake.Head.Y) == BORDER {
+		sc.Pg.Print()
 		return false
 	} else if sc.pg.GetContent(snake.Head.X, snake.Head.Y) == FOOD {
 		sc.pg.setRandomFood()
@@ -46,7 +45,7 @@ func Simulate(sc *SimpleSnakeController, move []DIRECTION, snakeLength int) bool
 	if snakeLength < 1 {
 		return true
 	} else {
-		return Simulate(sc, GetDirections(sc.pg.GetPlayGround()), depth, snakeLength-1)
+		return Simulate(sc, GetDirections(sc.Pg.GetPlayGround()), depth, snakeLength-1)
 	}
 	return Simulate(sc, GetDirections(sc.Pg.GetPlayGround()), depth, snakeLength-1)
 }
