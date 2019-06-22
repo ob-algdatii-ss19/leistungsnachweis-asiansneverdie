2bd5f5d64a07199c0eaf3994b88f206009adb41e
vmai <vmai@inovex.de>
2019-06-22 17:18:47 +0200

    try to fix

--- snake/simulation.go
+++ snake/simulation.go
@@ -20,12 +20,14 @@ func Simulate(sc *SimpleSnakeController, move []DIRECTION, depth, snakeLength in
 		duplicate := copyController.Pg.CopyPlayGround(copyController.Pg.GetPlayGround())
 		copyController.Snake.len = sc.Snake.len
 		copyController.Snake.LastDirection = sc.Snake.LastDirection
-		if Simulate(copyController, move[:1], depth+1, snakeLength) {
+		nextStep := copyController.getNextMovableFoodDirection(move)
+		nextArray := []DIRECTION{nextStep}
+		if Simulate(copyController, nextArray, depth+1, snakeLength) {
 			sc.Pg.SetPlayGround(duplicate)
-			sc.moveSnakeToFood(move[:1])
+			sc.moveSnakeToFood(nextArray)
 		} else {
 			sc.Pg.SetPlayGround(duplicate)
-			sc.moveSnakeToFood(move[1:])
+			sc.moveSnakeToFood(remove(move, nextStep))
 		}
 	} else {
 		sc.moveSnakeToFood(move)
@@ -47,7 +49,7 @@ func Simulate(sc *SimpleSnakeController, move []DIRECTION, depth, snakeLength in
 	}
 }
 
-func (sc *SimpleSnakeController) GetNextMovableFoodDirection(move []DIRECTION) DIRECTION {
+func (sc *SimpleSnakeController) getNextMovableFoodDirection(move []DIRECTION) DIRECTION {
 	dir := move[0]
 	var x, y = sc.Pg.GetFood()
 	if sc.Snake.Head.X < x && contains(move, RIGHT) {
@@ -62,10 +64,10 @@ func (sc *SimpleSnakeController) GetNextMovableFoodDirection(move []DIRECTION) D
 	return dir
 }
 
-func Remove(move []DIRECTION, element DIRECTION) []DIRECTION {
+func remove(move []DIRECTION, element DIRECTION) []DIRECTION {
 	movecopy := []DIRECTION{}
 	for ele := range move {
-		if DIRECTION(ele) != element {
+		if DIRECTION(ele) == element {
 			movecopy = append(movecopy, DIRECTION(ele))
 		}
 	}
