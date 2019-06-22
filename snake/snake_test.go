6660b96c8ee89ba6922c88aef00408d7d2ccc7e2
Reawel <colberg@hm.edu>
2019-06-22 17:36:31 +0200

    added snake test

--- snake/snake_test.go
+++ snake/snake_test.go
@@ -1,1 +1,13 @@
 package snake
+
+import "testing"
+
+func TestSnake(t *testing.T) {
+	snakeL := 5
+	snake := NewSnake(snakeL)
+
+	if snake.len != 5 {
+		t.Errorf("len of snake is: %d; want %d", snake.len, snakeL)
+	}
+
+}
